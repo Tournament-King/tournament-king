410915a56e8d8f4ac9d92554256e81e1e9d88728
Clayton Pabst <claytonpabst@gmail.com>
2017-09-01 17:07:11 -0600

    layout is there, need to write add function

--- t-king-client/src/components/Profile/CreateTournament.js
+++ t-king-client/src/components/Profile/CreateTournament.js
@@ -9,7 +9,11 @@ class CreateTournament extends Component {
 
         this.state = {
             handleNumberOfPlayersInput: 2,
-            matches: []
+            numberOfMatches: 1,
+            players: [
+                {id: null, name: 't1'},  
+                {id: null, name: 't2'},  
+            ]
         }
 
         this.handleNumberOfPlayers = this.handleNumberOfPlayers.bind(this);
@@ -17,8 +21,10 @@ class CreateTournament extends Component {
     }
 
     handleNumberOfPlayers(e) {
+        let input = parseInt(e.target.value)
         this.setState({
-            handleNumberOfPlayersInput: e.target.value
+            handleNumberOfPlayersInput: input,
+            numberOfMatches: input / 2
         })
     }
 
@@ -27,6 +33,17 @@ class CreateTournament extends Component {
 
     render() {
 
+        let matches = []
+        for(let i = 0; i<this.state.numberOfMatches; i++) {
+            matches.push(
+                <div className='ct-match'>
+                    <h1 className='ct-match-player1'>{this.state.players[((i+1)*2)-2].name}</h1>
+                    <h1 className='ct-match-player2'>{this.state.players[((i+1)*2)-1].name}</h1>
+                    {/*<h1>{this.state.player[((i+1)*2)-1].name}</h1>*/}
+                </div>
+            )
+        }
+        
         return (
             <main className='ct-main'>
                 <div className='ct-header'>
@@ -40,14 +57,14 @@ class CreateTournament extends Component {
                 <div className='ct-tournament-info'>
                     <form id="contact" action="" method="post">
                         <h3>Tournament Info</h3>
-                        <h4>!!!Maybe some cometition quote here?!!!</h4>
+                        <h4>!!!Maybe some competition quote here?!!!</h4>
                         <br />
 
-                        <input placeholder="Tournament Name" type="text" tabIndex="1" required/>
+                        <input placeholder="Tournament Name" type="text" required/>
 
-                        <textarea placeholder="Tournament Description (optional)" tabIndex="5"></textarea>
+                        <textarea placeholder="Tournament Description (optional)"></textarea>
 
-                        <select name="" id="" onChange={ (e) => this.handleNumberOfPlayers(e.target.value)}>
+                        <select value={this.state.handleNumberOfPlayersInput} onChange={this.handleNumberOfPlayers}>
                             <option value={2}>2 Competitors</option>
                             <option value={4}>4 Competitors</option>
                             <option value={8}>8 Competitors</option>
@@ -58,10 +75,19 @@ class CreateTournament extends Component {
                             <option value={256}>256 Competitors... Maybe rethink your life...</option>
                         </select>
                         <div className='ct-matches'>
-
+                            <br />
+                            <div className='ct-matches-header'>
+                                <h1>1st Round Matches</h1>
+                                {/*<br/>*/}
+                                <input style={{"width":"35%"}} type="text" placeholder='Competitor' tabIndex='1'/><span> vs </span>
+                                <input style={{"width":"35%"}} type="text" placeholder='Competitor' tabIndex='2'/>
+                                <button tabIndex='3' type='submit' style={{"margin":"10px"}} onClick={this.addPlayers}>Add</button>
+                            </div>
+                            <br />
+                            {matches}
                         </div>
 
-                        <div className='ct-submit'>Submit</div>
+                        <div className='ct-submit'>Finalize</div>
                    </form>
                 </div>
                 <div className='ct-player-info'>
