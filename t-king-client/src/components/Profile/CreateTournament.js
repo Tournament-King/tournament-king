578f0fe801eb9bafe3f341edc515e78a35292ec4
Clayton Pabst <claytonpabst@gmail.com>
2017-09-04 11:54:39 -0600

    create tournament view is almost done

--- t-king-client/src/components/Profile/CreateTournament.js
+++ t-king-client/src/components/Profile/CreateTournament.js
@@ -9,14 +9,273 @@ class CreateTournament extends Component {
 
         this.state = {
             handleNumberOfPlayersInput: 2,
+            player1Input: '',
+            player2Input: '',
             numberOfMatches: 1,
             players: [
-                {id: null, name: 't1'},  
-                {id: null, name: 't2'},  
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
+                {id: null, name: 'player'},
             ]
         }
 
         this.handleNumberOfPlayers = this.handleNumberOfPlayers.bind(this);
+        this.handlePlayer1Input = this.handlePlayer1Input.bind(this);
+        this.handlePlayer2Input = this.handlePlayer2Input.bind(this);
+        this.addPlayers = this.addPlayers.bind(this);
         //bind
     }
 
@@ -28,17 +287,51 @@ class CreateTournament extends Component {
         })
     }
 
+    handlePlayer1Input(e) {
+        this.setState({
+            player1Input: e.target.value
+        })
+    }
+
+    handlePlayer2Input(e) {
+        this.setState({
+            player2Input: e.target.value
+        })
+    }
+
+    addPlayers() {
+        console.log(this.state.numberOfMatches *2)
+        for(let i = 0; i < this.state.numberOfMatches * 2; i+=2) {
+            console.log(i)
+            if (this.state.players[i].name === 'player' && this.state.players[i+1].name ==='player') {
+                console.log('hit', i)
+                let newPlayers = [...this.state.players]
+                newPlayers[i].name = this.state.player1Input
+                newPlayers[i+1].name = this.state.player2Input
+                this.setState({
+                    players: newPlayers,
+                    player1Input: '',
+                    player2Input: ''
+                })
+                return
+            }
+        }    
+    }
+
 
 
 
     render() {
+        console.log(this.state)
 
         let matches = []
         for(let i = 0; i<this.state.numberOfMatches; i++) {
             matches.push(
                 <div className='ct-match'>
                     <h1 className='ct-match-player1'>{this.state.players[((i+1)*2)-2].name}</h1>
+                    <div className="ct-match-matchNumber">Match {i+1}</div>
                     <h1 className='ct-match-player2'>{this.state.players[((i+1)*2)-1].name}</h1>
+                    {/*<div onClick={() => this.editPlayers()}>Edit</div>*/}
                     {/*<h1>{this.state.player[((i+1)*2)-1].name}</h1>*/}
                 </div>
             )
@@ -55,7 +348,7 @@ class CreateTournament extends Component {
                     <p>each match for the fans to see.</p>
                 </div>
                 <div className='ct-tournament-info'>
-                    <form id="contact" action="" method="post">
+                    <div id="contact" action="">
                         <h3>Tournament Info</h3>
                         <h4>!!!Maybe some competition quote here?!!!</h4>
                         <br />
@@ -79,8 +372,21 @@ class CreateTournament extends Component {
                             <div className='ct-matches-header'>
                                 <h1>1st Round Matches</h1>
                                 {/*<br/>*/}
-                                <input style={{"width":"35%"}} type="text" placeholder='Competitor' tabIndex='1'/><span> vs </span>
-                                <input style={{"width":"35%"}} type="text" placeholder='Competitor' tabIndex='2'/>
+                                <input  style={{"width":"35%"}} 
+                                        type="text" 
+                                        placeholder='Competitor' 
+                                        value={this.state.player1Input}
+                                        onChange={this.handlePlayer1Input}
+                                        tabIndex='1'
+                                        />
+                                <span> vs </span>
+                                <input  style={{"width":"35%"}} 
+                                        type="text" 
+                                        placeholder='Competitor' 
+                                        value={this.state.player2Input}
+                                        onChange={this.handlePlayer2Input}                                        
+                                        tabIndex='2'
+                                        />
                                 <button tabIndex='3' type='submit' style={{"margin":"10px"}} onClick={this.addPlayers}>Add</button>
                             </div>
                             <br />
@@ -88,7 +394,7 @@ class CreateTournament extends Component {
                         </div>
 
                         <div className='ct-submit'>Finalize</div>
-                   </form>
+                   </div>
                 </div>
                 <div className='ct-player-info'>
 
