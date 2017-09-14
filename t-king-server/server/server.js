2352574dcc8e6ed1d7577883ae1609d304e66480
galaxy233 <vjmatonis@gmail.com>
2017-09-14 16:59:10 -0600

    remove unused endpoints and middleware

--- t-king-server/server/server.js
+++ t-king-server/server/server.js
@@ -22,11 +22,6 @@ const userCtrl = require('./controllers/user-controller');
 const matchCtrl = require('./controllers/match-controller');
 const tournamentCtrl = require('./controllers/tournament-controller');
 const searchCtrl = require('./controllers/search-controller');
-const commentCtrl = require('./controllers/comment-controller');
-
-//-------------------------MIDDLEWARE--------------------------//
-
-const authMiddleware = require('./middleware/authorization');
 
 //--------------------------APP SETUP---------------------------//
 
@@ -113,10 +108,6 @@ app.get('/api/tournament/:id', tournamentCtrl.getTournament);
 app.post('/api/tournament', tournamentCtrl.createTournament);
 
 app.get('/api/match/:id', matchCtrl.getMatch);
-app.post('/api/match/setwinner', matchCtrl.setWinner);
-
-app.get('/api/comments/:match_id', commentCtrl.getComments);
-app.post('/api/comment', commentCtrl.createComment);
 
 app.get('/api/search/users', searchCtrl.users);
 
